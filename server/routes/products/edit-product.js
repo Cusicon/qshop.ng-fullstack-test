44d44fd5d9f68955357298d1ba7e1c8ca3209fdb
Success Chukwu <iamcusicon@gmail.com>
2022-02-03 23:33:37 +0100

    editing a product done

--- server/routes/products/edit-product.js
+++ server/routes/products/edit-product.js
@@ -4,41 +4,70 @@ const { body, validationResult } = require('express-validator')
 
 // --( MODELS )--
 const Product = require('../../models/product')
-const Category = require('../../models/category')
 
-// PUT: all products
-router.put('/:slug', async (req, res) => {
-  const { slug } = req.params
+// PUT: edit product
+router.put(
+  '/:id',
+  body('description', 'description is important!').notEmpty().isString(),
+  body('price', 'input a price!').notEmpty().isFloat(),
+  body('qty', 'enter its quantity!').notEmpty().isInt(),
+  async (req, res) => {
+    req.stringifyBody(req.body)
 
-  try {
-    let product = await Product.findOne({ slug }).lean().exec()
+    const { id: prod_id } = req.params
+    const allowedBody = ['description', 'price', 'qty']
+    req.cleanBody(allowedBody)
 
-    if (!product)
+    const errors = validationResult(req)
+
+    // Checking for Errors
+    if (!errors.isEmpty())
       return res.json({
         ...global.jsonBag,
-        status: (res.statusCode = 401),
-        message: 'Sorry, no product found!',
-        error: null,
+        status: (res.statusCode = 400),
+        error: {
+          message: "Hello? You've entered some invalid fields!",
+          data: errors.array(),
+        },
         data: null,
       })
 
-    return res.json({
-      ...global.jsonBag,
-      message: 'Updated a product!',
-      data: { ...product },
-      error: null,
-    })
-  } catch (err) {
-    return res.json({
-      ...global.jsonBag,
-      status: (res.statusCode = err.status || 500),
-      error: {
-        message: err.message,
-        data: { ...err },
-      },
-      data: null,
-    })
+    // Edit product
+    try {
+      let newUpdate = { ...req.body }
+      let product = await Product.findByIdAndUpdate(prod_id, {
+        ...newUpdate,
+      })
+        .lean()
+        .exec()
+
+      if (!product)
+        return res.json({
+          ...global.jsonBag,
+          status: (res.statusCode = 401),
+          message: 'Sorry, no product found!',
+          error: null,
+          data: null,
+        })
+
+      return res.json({
+        ...global.jsonBag,
+        message: 'Updated a product!',
+        data: { ...product },
+        error: null,
+      })
+    } catch (err) {
+      return res.json({
+        ...global.jsonBag,
+        status: (res.statusCode = err.status || 500),
+        error: {
+          message: err.message,
+          data: { ...err },
+        },
+        data: null,
+      })
+    }
   }
-})
+)
 
 module.exports = router
