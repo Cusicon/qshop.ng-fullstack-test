93a9f3326188a22fba805dd2ca18147af733b646
Success Chukwu <iamcusicon@gmail.com>
2022-02-03 18:08:00 +0100

    renamed all to all-category

--- server/routes/category/index.js
+++ server/routes/category/index.js
@@ -7,7 +7,7 @@ const jwtVerify = require('../../utils/jwtVerify')
 // USE: verify every user in every routes
 router.use('*', jwtVerify, onlyRole(['user']))
 
-router.use('/', require('./all')) // View all categories
+router.use('/', require('./all-category')) // View all categories
 router.use('/add', require('./add-category')) // Add a category
 // router.use('/update', require('./update-category')) // Update a category
 // router.use('/delete', require('./delete-category')) // Delete a category
