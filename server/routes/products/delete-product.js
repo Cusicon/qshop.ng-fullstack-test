fd4682d436558c61beca31b7702976b17b5100e6
Success Chukwu <iamcusicon@gmail.com>
2022-02-04 01:06:04 +0100

    deleting a product is done

--- server/routes/products/delete-product.js
+++ server/routes/products/delete-product.js
@@ -1,18 +1,43 @@
 const { Router } = require('express')
 const router = Router()
-const { body, validationResult } = require('express-validator')
 
 // --( MODELS )--
 const Product = require('../../models/product')
 
 // DELETE: delete product
-router.delete('/:id', (req, res) => {
-  return res.json({
-    ...global.jsonBag,
-    message: 'Deleted a product!',
-    error: null,
-    data: null,
-  })
+router.delete('/:id', async (req, res) => {
+  const { id: prod_id } = req.params
+
+  // Delete product
+  try {
+    let product = await Product.findByIdAndDelete(prod_id).lean().exec()
+
+    if (!product)
+      return res.json({
+        ...global.jsonBag,
+        status: (res.statusCode = 401),
+        message: 'Sorry, no product found!',
+        error: null,
+        data: null,
+      })
+
+    return res.json({
+      ...global.jsonBag,
+      message: 'Deleted a product!',
+      data: { ...product },
+      error: null,
+    })
+  } catch (err) {
+    return res.json({
+      ...global.jsonBag,
+      status: (res.statusCode = err.status || 500),
+      error: {
+        message: err.message,
+        data: { ...err },
+      },
+      data: null,
+    })
+  }
 })
 
 module.exports = router
